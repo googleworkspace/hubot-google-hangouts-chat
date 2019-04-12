0add2ce5af1c1033d65b5617fc8efffdee98aa35
Reyno Tilikaynen <reyno@google.com>
2019-04-12 11:59:45 -0400

    Adding support for asynchronously messaging rooms.
    There are now two flows for sending messages:
     * Responding to a message: specify the envelope.message parameter, in
       which case the bot will respond to the message. This is the existing
       behavior and is used by the `send` and `reply` methods.
     * Messaging a space: specify the space name in the envelope.room
       parameter, in which case the bot will message that space. This is
       used by the `messageRoom` method.
    
    For HTTP bots, messaging a space will require authentication using a service
    account as it uses the REST API to perform the request.
    
    Change-Id: Id7ef9b047e9d63f6491373c29b0babbbacb09b53

--- src/bot.js
+++ src/bot.js
@@ -30,63 +30,94 @@ class HangoutsChatBot extends Adapter {
   constructor(robot, options) {
     super(robot);
 
-    this.subscriptionName = `projects/${options.projectId}/subscriptions/${options.subscriptionId}`;
+    this.subscriptionName =
+        `projects/${options.projectId}/subscriptions/${options.subscriptionId}`;
     this.isPubSub = options.isPubSub;
     this.port = options.port;
 
-    if (this.isPubSub) {
-      // Establish OAuth with Hangouts Chat
-      let authClientPromise = auth.getClient({
-        scopes: ['https://www.googleapis.com/auth/chat.bot']
-      });
-      this.chatPromise = authClientPromise.then((credentials) =>
-        google.chat({
-          version: 'v1',
-          auth: credentials
-        }));
-      }
+    // Establish OAuth with Hangouts Chat. This is required for PubSub bots and
+    // HTTP bots which want to create async messages.
+    let authClientPromise = auth.getClient({
+      scopes: ['https://www.googleapis.com/auth/chat.bot']
+    });
+    this.chatPromise = authClientPromise.then((credentials) =>
+      google.chat({
+        version: 'v1',
+        auth: credentials
+      })).catch((err) =>
+        robot.logger.warning("Hangouts Chat Authentication Failed! This may " +
+          "cause message creation to fail. Please provide the credentials " +
+          "for your service account.\n" + err));
   }
 
-  /*
+  /**
    * Helper method which takes care of constructing the response url
    * and sending the message to Hangouts Chat.
+   *
+   * This method has different behavior based on the type of envelope passed in.
+   * <ul>
+   *   <li>If the envelope has a message, it responds to the message. For HTTP
+   *       bots, an HTTP response is sent.
+   *   <li>If the envelope does not have a message, it must have the room
+   *       parameter set to the name of a space. The bot will use the REST API
+   *       to post a message in the space.
+   * </ul>
+   * @param {Object} envelope An Object with message, room, and user details.
+   * @param {string} text The message text.
    */
-  sendMessage(envelope, ...strings) {
-    let data = {
-        text: strings[0] || '',
-        thread: envelope.message.thread
-    };
+  sendMessage_(envelope, text = '') {
+    let hasMessage = !!envelope.message;
+    if (!hasMessage && !envelope.room) {
+      throw new Error("When sending a message, the envelope must have either " +
+          "a message or a room.");
+    }
 
-    if (this.isPubSub) {
-      this.chatPromise.then((chat) => 
-        chat.spaces.messages.create({
-            parent: envelope.message.space.name,
-            requestBody: data
-        }));
-    } else {
-      // Post message as HTTP response
+    let spaceName = hasMessage ? envelope.message.space.name : envelope.room;
+    let data = {
+      space: {
+        name: spaceName,
+      },
+      text,
+    };
+    if (hasMessage) {
+      data.thread = envelope.message.thread;
+    }
+
+    this.robot.logger.info("Sending a message to space: " + spaceName);
+    if (hasMessage && envelope.message.httpRes) {
       envelope.message.httpRes.json(data);
+    } else {
+      this.createMessageUsingRestApi_(spaceName, data);
     }
   }
 
-  /*
-   * Hubot is sending a message to Hangouts Chat.
-   */
-  send (envelope, ...strings) {
-    this.sendMessage(envelope, ...strings);
+  /** Hubot is sending a message to Hangouts Chat. */
+  send(envelope, ...strings) {
+    this.sendMessage_(envelope, strings[0] || '');
   }
 
-  /*
-   * Hubot is sending a reply to Hangouts Chat.
-   */
-  reply (envelope, ...strings) {
-    this.sendMessage(envelope, ...strings);
+  /** Hubot is sending a reply to Hangouts Chat. */
+  reply(envelope, ...strings) {
+    this.sendMessage_(envelope, strings[0] || '');
   }
 
-  /*
-   * Activates HTTP listener and sets
-   * up handler to handle events.
+  /**
+   * Helper method for creating a message using the Hangouts Chat REST API.
+   * @param {string} space The space in which the message should be created.
+   * @param {Object} message The Message REST resource that should be added.
    */
+  createMessageUsingRestApi_(space, message) {
+    this.chatPromise.then((chat) =>
+        chat.spaces.messages.create({
+            parent: space,
+            requestBody: message
+        }))
+        .catch((err) =>
+            this.robot.logger.error("Message creation failed. This may be " +
+                "due to missing service account credentials.", err));
+  }
+
+  /** Activates HTTP listener and sets up handler to handle events. */
   startHttpServer() {
     // Create an event handler to handle messages.
     app.post('/', (req, res) => {
@@ -98,21 +129,22 @@ class HangoutsChatBot extends Adapter {
     });
   }
 
-  /*
-   * Initializes the Cloud Pub/Sub Subscriber,
-   * establishes connection to the Subscription
-   * and sets up handler to handle events.
+  /**
+   * Initializes the Cloud Pub/Sub Subscriber, establishes connection to the
+   * Subscription and sets up handler to handle events.
    */
   startPubSubClient() {
     const pubsub = PubSub();
-    this.robot.logger.info(`Connecting to Pub/Sub subscription - ${this.subscriptionName}`);
+    this.robot.logger.info(
+        `Connecting to Pub/Sub subscription - ${this.subscriptionName}`);
     const subscription = pubsub.subscription(this.subscriptionName);
     // Create an event handler to handle messages.
     const messageHandler = (pubsubMessage) => {
       this.robot.logger.debug(`Received message ${pubsubMessage.id}:`);
       this.robot.logger.debug(`\tData: ${pubsubMessage.data}`);
 
-      const dataUtf8encoded = Buffer.from(pubsubMessage.data, 'base64').toString('utf8');
+      const dataUtf8encoded =
+          Buffer.from(pubsubMessage.data, 'base64').toString('utf8');
       let event;
       try {
         event = JSON.parse(dataUtf8encoded);
@@ -132,13 +164,12 @@ class HangoutsChatBot extends Adapter {
   }
 
 
-  /*
-   * Invoked when Event is received from Hangouts Chat.
-   */
+  /** Invoked when Event is received from Hangouts Chat. */
   onEventReceived(event, res) {
     // TODO(kavimehta): Create and allow for additional message types
     if (event.type != 'MESSAGE') {
       this.robot.logger.info('Only MESSAGE event supported now !');
+      this.robot.logger.info(event);
       return;
     }
     let message = event.message;
@@ -162,10 +193,9 @@ class HangoutsChatBot extends Adapter {
     this.robot.receive(hangoutsChatTextMessage);
   }
 
-  /*
-   * This is like the main method.
-   * Hubot invokes this method, when we use this adapter, to setup
-   * the bridge between Hubot and Hangouts Chat
+  /**
+   * Sets up adapter for communicating with Hangouts Chat. Hubot invokes this
+   * message during initialization.
    */
   run() {
     if (this.isPubSub) {
@@ -180,7 +210,6 @@ class HangoutsChatBot extends Adapter {
     // To make Hubot load scripts
     this.emit('connected')
   }
-
 }
 
 module.exports = HangoutsChatBot
